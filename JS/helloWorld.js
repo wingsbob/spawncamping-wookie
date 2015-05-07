07cc09da689e8c3fec9f78da8344cff0d85dec35
hctpscl <rachel.quigley@gmail.com>
2015-05-07 22:57:06 +0100

    oh god wat do i do

--- JS/helloWorld.js
+++ JS/helloWorld.js
@@ -2,10 +2,12 @@
 	'use strict';
 
 	function HelloWorld (options) {
+		this.options = options || {};
 	}
 
 	HelloWorld.prototype.sendToConsole = function(message) {
-		// body...
+		this.defaultmessage = 'my default message';
+		return message || this.defaultMessage;
 	};
 
 	module.exports = HelloWorld;
